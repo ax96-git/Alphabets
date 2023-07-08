5e5afd9709a7796a584ebeed09413593d4decab3
mohammed <70203540+ax96-git@users.noreply.github.com>
2023-07-08 13:22:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,360 @@
+// var a1=document.getElementById("a1");
+// var b1=document.getElementById("b1");
+// var c1=document.getElementById("c1");
+// var d1=document.getElementById("d1");
+// var e1=document.getElementById("e1");
+
+// var a = new Audio('..\\audio\\a.mp3');
+// var b = new Audio('..\\audio\\b.mp3');
+// var c = new Audio('..\\audio\\c.mp3');
+// var d = new Audio('..\\audio\\d.mp3');
+// var ee = new Audio('..\\audio\\e.mp3');
+
+
+
+
+// var a1_sty=window.getComputedStyle(a1).fontSize; 
+// console.log('hello')
+// document.body.addEventListener('keypress',function(e){
+// var keycodee=e.key
+// if(keycodee==="a"){a1.style.fontSize="70px";a1.style.color="gold"; a.play();
+// setTimeout(function() {a1.style.fontSize = "";a1.style.color = "";}, 500);}
+// else if(keycodee==="b"){b1.style.fontSize="70px";b1.style.color="gold"; b.play();
+// setTimeout(function() {b1.style.fontSize = "";b1.style.color = "";}, 500);}
+// else if(keycodee==="c"){c1.style.fontSize="70px";c1.style.color="gold"; c.play();
+// setTimeout(function() {c1.style.fontSize = "";c1.style.color = "";}, 500);}
+// else if(keycodee==="d"){d1.style.fontSize="70px";d1.style.color="gold"; d.play();
+// setTimeout(function() {d1.style.fontSize = "";d1.style.color = "";}, 500);}
+// else if(keycodee==="e"){e1.style.fontSize="70px";e1.style.color="gold"; ee.play();
+// setTimeout(function() {e1.style.fontSize = "";e1.style.color = "";}, 500);}
+
+
+
+
+// })
+
+
+var a1 = document.getElementById("a1");
+var b1 = document.getElementById("b1");
+var c1 = document.getElementById("c1");
+var d1 = document.getElementById("d1");
+var e1 = document.getElementById("e1");
+var f1 = document.getElementById("f1");
+var g1 = document.getElementById("g1");
+var h1 = document.getElementById("h1");
+var i1 = document.getElementById("i1");
+var j1 = document.getElementById("j1");
+var k1 = document.getElementById("k1");
+var l1 = document.getElementById("l1");
+var m1 = document.getElementById("m1");
+var n1 = document.getElementById("n1");
+var o1 = document.getElementById("o1");
+var p1 = document.getElementById("p1");
+var q1 = document.getElementById("q1");
+var r1 = document.getElementById("r1");
+var s1 = document.getElementById("s1");
+var t1 = document.getElementById("t1");
+var u1 = document.getElementById("u1");
+var v1 = document.getElementById("v1");
+var w1 = document.getElementById("w1");
+var x1 = document.getElementById("x1");
+var y1 = document.getElementById("y1");
+var z1 = document.getElementById("z1");
+
+var a = new Audio('..\\audio\\a.mp3');
+var b = new Audio('..\\audio\\b.mp3');
+var c = new Audio('..\\audio\\c.mp3');
+var d = new Audio('..\\audio\\d.mp3');
+var ee = new Audio('..\\audio\\e.mp3');
+var f = new Audio('..\\audio\\f.mp3');
+var g = new Audio('..\\audio\\g.mp3');
+var h = new Audio('..\\audio\\h.mp3');
+var i = new Audio('..\\audio\\i.mp3');
+var j = new Audio('..\\audio\\j.mp3');
+var k = new Audio('..\\audio\\k.mp3');
+var l = new Audio('..\\audio\\l.mp3');
+var m = new Audio('..\\audio\\m.mp3');
+var n = new Audio('..\\audio\\n.mp3');
+var o = new Audio('..\\audio\\o.mp3');
+var p = new Audio('..\\audio\\p.mp3');
+var q = new Audio('..\\audio\\q.mp3');
+var r = new Audio('..\\audio\\r.mp3');
+var s = new Audio('..\\audio\\s.mp3');
+var t = new Audio('..\\audio\\t.mp3');
+var u = new Audio('..\\audio\\u.mp3');
+var v = new Audio('..\\audio\\v.mp3');
+var w = new Audio('..\\audio\\w.mp3');
+var x = new Audio('..\\audio\\x.mp3');
+var y = new Audio('..\\audio\\y.mp3');
+var z = new Audio('..\\audio\\z.mp3');
+
+
+a1.addEventListener('mouseover',function(){a.play();})
+b1.addEventListener('mouseover',function(){b.play();})
+c1.addEventListener('mouseover',function(){c.play();})
+d1.addEventListener('mouseover',function(){d.play();})
+e1.addEventListener('mouseover',function(){ee.play();})
+f1.addEventListener('mouseover',function(){f.play();})
+g1.addEventListener('mouseover',function(){g.play();})
+h1.addEventListener('mouseover',function(){h.play();})
+i1.addEventListener('mouseover',function(){i.play();})
+j1.addEventListener('mouseover',function(){j.play();})
+k1.addEventListener('mouseover',function(){k.play();})
+l1.addEventListener('mouseover',function(){l.play();})
+m1.addEventListener('mouseover',function(){m.play();})
+n1.addEventListener('mouseover',function(){n.play();})
+o1.addEventListener('mouseover',function(){o.play();})
+p1.addEventListener('mouseover',function(){p.play();})
+q1.addEventListener('mouseover',function(){q.play();})
+r1.addEventListener('mouseover',function(){r.play();})
+s1.addEventListener('mouseover',function(){s.play();})
+t1.addEventListener('mouseover',function(){t.play();})
+u1.addEventListener('mouseover',function(){u.play();})
+v1.addEventListener('mouseover',function(){v.play();})
+w1.addEventListener('mouseover',function(){w.play();})
+x1.addEventListener('mouseover',function(){x.play();})
+y1.addEventListener('mouseover',function(){y.play();})
+z1.addEventListener('mouseover',function(){z.play();})
+
+
+document.body.addEventListener('keydown', function(e) {
+    var keycode = e.key.toLowerCase();
+
+    if (keycode === "a") {
+        a1.style.fontSize = "70px";
+        a1.style.color = "gold";
+        a.play();
+
+        setTimeout(function() {
+            a1.style.fontSize = "";
+            a1.style.color = "";
+        }, 500);
+    } else if (keycode === "b") {
+        b1.style.fontSize = "70px";
+        b1.style.color = "gold";
+        b.play();
+
+        setTimeout(function() {
+            b1.style.fontSize = "";
+            b1.style.color = "";
+        }, 500);
+    } else if (keycode === "c") {
+        c1.style.fontSize = "70px";
+        c1.style.color = "gold";
+        c.play();
+
+        setTimeout(function() {
+            c1.style.fontSize = "";
+            c1.style.color = "";
+        }, 500);
+    } else if (keycode === "d") {
+        d1.style.fontSize = "70px";
+        d1.style.color = "gold";
+        d.play();
+
+        setTimeout(function() {
+            d1.style.fontSize = "";
+            d1.style.color = "";
+        }, 500);
+    } else if (keycode === "e") {
+        e1.style.fontSize = "70px";
+        e1.style.color = "gold";
+        ee.play();
+
+        setTimeout(function() {
+            e1.style.fontSize = "";
+            e1.style.color = "";
+        }, 500);
+    } else if (keycode === "f") {
+        f1.style.fontSize = "70px";
+        f1.style.color = "gold";
+        f.play();
+
+        setTimeout(function() {
+            f1.style.fontSize = "";
+            f1.style.color = "";
+        }, 500);
+    } else if (keycode === "g") {
+        g1.style.fontSize = "70px";
+        g1.style.color = "gold";
+        g.play();
+
+        setTimeout(function() {
+            g1.style.fontSize = "";
+            g1.style.color = "";
+        }, 500);
+    } else if (keycode === "h") {
+        h1.style.fontSize = "70px";
+        h1.style.color = "gold";
+        h.play();
+
+        setTimeout(function() {
+            h1.style.fontSize = "";
+            h1.style.color = "";
+        }, 500);
+    } else if (keycode === "i") {
+        i1.style.fontSize = "70px";
+        i1.style.color = "gold";
+        i.play();
+
+        setTimeout(function() {
+            i1.style.fontSize = "";
+            i1.style.color = "";
+        }, 500);
+    } else if (keycode === "j") {
+        j1.style.fontSize = "70px";
+        j1.style.color = "gold";
+        j.play();
+
+        setTimeout(function() {
+            j1.style.fontSize = "";
+            j1.style.color = "";
+        }, 500);
+    } else if (keycode === "k") {
+        k1.style.fontSize = "70px";
+        k1.style.color = "gold";
+        k.play();
+
+        setTimeout(function() {
+            k1.style.fontSize = "";
+            k1.style.color = "";
+        }, 500);
+    } else if (keycode === "l") {
+        l1.style.fontSize = "70px";
+        l1.style.color = "gold";
+        l.play();
+
+        setTimeout(function() {
+            l1.style.fontSize = "";
+            l1.style.color = "";
+        }, 500);
+    } else if (keycode === "m") {
+        m1.style.fontSize = "70px";
+        m1.style.color = "gold";
+        m.play();
+
+        setTimeout(function() {
+            m1.style.fontSize = "";
+            m1.style.color = "";
+        }, 500);
+    } else if (keycode === "n") {
+        n1.style.fontSize = "70px";
+        n1.style.color = "gold";
+        n.play();
+
+        setTimeout(function() {
+            n1.style.fontSize = "";
+            n1.style.color = "";
+        }, 500);
+    } else if (keycode === "o") {
+        o1.style.fontSize = "70px";
+        o1.style.color = "gold";
+        o.play();
+
+        setTimeout(function() {
+            o1.style.fontSize = "";
+            o1.style.color = "";
+        }, 500);
+    } else if (keycode === "p") {
+        p1.style.fontSize = "70px";
+        p1.style.color = "gold";
+        p.play();
+
+        setTimeout(function() {
+            p1.style.fontSize = "";
+            p1.style.color = "";
+        }, 500);
+    } else if (keycode === "q") {
+        q1.style.fontSize = "70px";
+        q1.style.color = "gold";
+        q.play();
+
+        setTimeout(function() {
+            q1.style.fontSize = "";
+            q1.style.color = "";
+        }, 500);
+    } else if (keycode === "r") {
+        r1.style.fontSize = "70px";
+        r1.style.color = "gold";
+        r.play();
+
+        setTimeout(function() {
+            r1.style.fontSize = "";
+            r1.style.color = "";
+        }, 500);
+    } else if (keycode === "s") {
+        s1.style.fontSize = "70px";
+        s1.style.color = "gold";
+        s.play();
+
+        setTimeout(function() {
+            s1.style.fontSize = "";
+            s1.style.color = "";
+        }, 500);
+    } else if (keycode === "t") {
+        t1.style.fontSize = "70px";
+        t1.style.color = "gold";
+        t.play();
+
+        setTimeout(function() {
+            t1.style.fontSize = "";
+            t1.style.color = "";
+        }, 500);
+    } else if (keycode === "u") {
+        u1.style.fontSize = "70px";
+        u1.style.color = "gold";
+        u.play();
+
+        setTimeout(function() {
+            u1.style.fontSize = "";
+            u1.style.color = "";
+        }, 500);
+    } else if (keycode === "v") {
+        v1.style.fontSize = "70px";
+        v1.style.color = "gold";
+        v.play();
+
+        setTimeout(function() {
+            v1.style.fontSize = "";
+            v1.style.color = "";
+        }, 500);
+    } else if (keycode === "w") {
+        w1.style.fontSize = "70px";
+        w1.style.color = "gold";
+        w.play();
+
+        setTimeout(function() {
+            w1.style.fontSize = "";
+            w1.style.color = "";
+        }, 500);
+    } else if (keycode === "x") {
+        x1.style.fontSize = "70px";
+        x1.style.color = "gold";
+        x.play();
+
+        setTimeout(function() {
+            x1.style.fontSize = "";
+            x1.style.color = "";
+        }, 500);
+    } else if (keycode === "y") {
+        y1.style.fontSize = "70px";
+        y1.style.color = "gold";
+        y.play();
+
+        setTimeout(function() {
+            y1.style.fontSize = "";
+            y1.style.color = "";
+        }, 500);
+    } else if (keycode === "z") {
+        z1.style.fontSize = "70px";
+        z1.style.color = "gold";
+        z.play();
+
+        setTimeout(function() {
+            z1.style.fontSize = "";
+            z1.style.color = "";
+        }, 500);
+    }
+});
+
+
